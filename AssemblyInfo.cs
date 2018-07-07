4ea7e7b4510806325bf0f629517130b6e1a97eb5
Vladas Cibulskis <vladas.cibulskis@gmail.com>
2018-07-07 22:12:44 +0300

    Re-branding to MBitcoinCash.ProjectSetup. Adding FxCopAnalyzers.

---
+++ AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿// <copyright file="AssemblyInfo.cs" company="Modular Bitcoin Cash">
+// Copyright (c) 2018-2018 Modular Bitcoin Cash developers.
+// Distributed under the MIT software license, see the accompanying LICENSE file in the project root
+// or http://www.opensource.org/licenses/mit-license.php for full license information.
+// </copyright>
+
+using System;
+[assembly: CLSCompliant(true)]
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# MBitcoinCash.MerkleTree
-Generic utility to build and validate Merkle Trees
+# MBitcoinCash.ProjectSetup
+
+This repository is used to configure project setup that applies to all sub-projects in the MBitcoinCash family.
